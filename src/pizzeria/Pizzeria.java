4db2a0997445422c10bedc2c2f7690501752dfc1
Javega1 <Ja.vega@Duocuc.cl>
2025-09-03 16:20:03 -0400

    ...

--- src/pizzeria/Pizzeria.java
+++ src/pizzeria/Pizzeria.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pizzeria;
 
-/**
- *
- * @author CETECOM
- */
+
 public class Pizzeria {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         Pizza pizza1 = new Pizza();
         
         String captura = pizza1.toString();
